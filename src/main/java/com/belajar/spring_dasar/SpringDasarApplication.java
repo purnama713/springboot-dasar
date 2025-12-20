f65432c74784cb7064ad2cba66af4e3ad2102ce2
purnama713 <komarudinpurnama@gmail.com>
2025-12-20 10:38:12 +0700

    Initial commit

---
+++ src/main/java/com/belajar/spring_dasar/SpringDasarApplication.java
@@ -0,0 +1,13 @@
+package com.belajar.spring_dasar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDasarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDasarApplication.class, args);
+	}
+
+}
