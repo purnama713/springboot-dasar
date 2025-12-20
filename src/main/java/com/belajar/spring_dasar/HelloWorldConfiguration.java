956320ac6f35ea541ba136bf70c253ca269c8949
purnama713 <komarudinpurnama@gmail.com>
2025-12-20 15:40:27 +0700

    Application Context Test

---
+++ src/main/java/com/belajar/spring_dasar/HelloWorldConfiguration.java
@@ -0,0 +1,9 @@
+package com.belajar.spring_dasar;
+
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class HelloWorldConfiguration {
+
+}
---
+++ src/test/java/com/belajar/spring_dasar/ApplicationContextTest.java
@@ -0,0 +1,17 @@
+package com.belajar.spring_dasar;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextTest {
+
+    @Test
+    void testApplicationContext(){
+        // Polymorphism
+        ApplicationContext context = new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
+
+        Assertions.assertNotNull(context);
+    }
+}
