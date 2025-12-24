f57a149224cbd20ad802283cd0530f120cbfd1bc
purnama713 <komarudinpurnama@gmail.com>
2025-12-24 13:51:06 +0700

    Depends On Test

---
+++ src/main/java/com/belajar/spring_dasar/DependsOnConfiguration.java
@@ -0,0 +1,26 @@
+package com.belajar.spring_dasar;
+
+import com.belajar.spring_dasar.data.Bar;
+import com.belajar.spring_dasar.data.Foo;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.DependsOn;
+
+@Slf4j
+@Configuration
+public class DependsOnConfiguration {
+
+    @Bean
+    @DependsOn({ "bar"})
+    public Foo foo() {
+        log.info("Create Foo");
+        return new Foo();
+    }
+
+    @Bean
+    public Bar bar() {
+        log.info("Create Bar");
+        return new Bar();
+    }
+}
---
+++ src/test/java/com/belajar/spring_dasar/DependsOnTest.java
@@ -0,0 +1,21 @@
+package com.belajar.spring_dasar;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class DependsOnTest {
+
+    private ApplicationContext applicationContext;
+
+    @BeforeEach
+    void setUp() {
+        applicationContext = new AnnotationConfigApplicationContext(DependsOnConfiguration.class);
+    }
+
+    @Test
+    void testDependsOn() {
+        // Test untuk memastikan bahwa bean dengan dependsOn diinisialisasi terlebih dahulu
+    }
+}
