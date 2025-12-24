2fe30fe2889e93caf7dc0fe6ebea0bdd5917d102
purnama713 <komarudinpurnama@gmail.com>
2025-12-24 13:12:32 +0700

    Bean Name Test

---
+++ src/main/java/com/belajar/spring_dasar/BeanNameConfiguration.java
@@ -0,0 +1,22 @@
+package com.belajar.spring_dasar;
+
+import com.belajar.spring_dasar.data.Foo;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class BeanNameConfiguration {
+
+    @Primary
+    @Bean(name = "fooFirst")
+    public Foo foo1() {
+        return new Foo();
+    }
+
+    @Bean(name = "fooSecond")
+    public Foo foo2() {
+        return new Foo();
+    }
+
+}
