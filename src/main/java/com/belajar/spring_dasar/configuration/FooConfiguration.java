4864caaed9f4c29673ac28fe0efb191667b5e606
purnama713 <komarudinpurnama@gmail.com>
2025-12-26 18:19:35 +0700

    Import Configuration, Component Scan, and Object Provider DI Test

--- src/main/java/com/belajar/spring_dasar/configuration/FooConfiguration.java
+++ src/main/java/com/belajar/spring_dasar/configuration/FooConfiguration.java
@@ -3,12 +3,25 @@ package com.belajar.spring_dasar.configuration;
 import com.belajar.spring_dasar.data.Foo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class FooConfiguration {
 
     @Bean
+    @Primary // Agar tidak merusak unit test sebelumnya
     public Foo foo() {
+        return new Foo();
+    }
+
+    // Object Provider
+    @Bean
+    public Foo foo2() {
+        return new Foo();
+    }
+
+    @Bean
+    public Foo foo3() {
         return new Foo();
     }
 
