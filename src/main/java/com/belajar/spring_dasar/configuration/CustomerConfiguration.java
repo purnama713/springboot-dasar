1a4f43efd3263b2f41e7c9f0a78d3093729e28b0
purnama713 <komarudinpurnama@gmail.com>
2025-12-26 15:55:38 +0700

    Qualifier Test

---
+++ src/main/java/com/belajar/spring_dasar/configuration/CustomerConfiguration.java
@@ -0,0 +1,22 @@
+package com.belajar.spring_dasar.configuration;
+
+import com.belajar.spring_dasar.repository.CustomerRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class CustomerConfiguration {
+
+    @Primary
+    @Bean
+    public CustomerRepository normalCustomerRepository() {
+        return new CustomerRepository();
+    }
+
+    @Bean
+    public CustomerRepository premiumCustomerRepository() {
+        return new CustomerRepository();
+    }
+}
