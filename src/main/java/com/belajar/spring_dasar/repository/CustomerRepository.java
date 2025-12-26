561d8f5be5f2d5750bdf17ce2fa37edd99847452
purnama713 <komarudinpurnama@gmail.com>
2025-12-26 15:55:23 +0700

    Field-based DI, and Qualifier Test

--- src/main/java/com/belajar/spring_dasar/repository/CustomerRepository.java
+++ src/main/java/com/belajar/spring_dasar/repository/CustomerRepository.java
@@ -2,6 +2,6 @@ package com.belajar.spring_dasar.repository;
 
 import org.springframework.stereotype.Component;
 
-@Component
+//@Component // hapus jika menggunakan dua bean di CustomerConfiguration
 public class CustomerRepository {
 }
--- src/main/java/com/belajar/spring_dasar/service/CustomerService.java
+++ src/main/java/com/belajar/spring_dasar/service/CustomerService.java
@@ -3,12 +3,25 @@ package com.belajar.spring_dasar.service;
 import com.belajar.spring_dasar.repository.CustomerRepository;
 import lombok.Getter;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CustomerService {
 
+    // Field Dependency Injection
+//    @Getter
+//    @Autowired
+//    private CustomerRepository customerRepository;
+
+    // Menggunakan Qualifier untuk membedakan dua bean CustomerRepository
     @Getter
     @Autowired
-    private CustomerRepository customerRepository;
+    @Qualifier("normalCustomerRepository")
+    private CustomerRepository normalCustomerRepository;
+
+    @Getter
+    @Autowired
+    @Qualifier("premiumCustomerRepository")
+    private CustomerRepository premiumCustomerRepository;
 }
